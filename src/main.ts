9e94f059ff8dac42961dd9de0862a770329226fe
Cheristic <ethanheffan@gmail.com>
2024-10-11 21:51:31 -0700

    Step 6 multiple smusher upgrades

--- src/main.ts
+++ src/main.ts
@@ -4,128 +4,189 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My freaking sweet lil game";
 document.title = gameName;
 
-const header = document.createElement("div");
-header.setAttribute("id", "head");
-header.setAttribute(
-  "style",
-  "vertical-align: text-top;font-size: 1em;text-align: center;",
-);
-document.body.prepend(header);
-const headerText = document.createElement("h1");
-headerText.innerHTML = gameName;
-header.append(headerText);
-
-const centerContainer = document.createElement("div");
-app.append(centerContainer);
-const smushCounter = document.createElement("div");
-smushCounter.style.fontSize = "35px";
-smushCounter.innerHTML = "0";
-centerContainer.append(smushCounter);
-
-app.append(document.createElement("br"));
-
-const miceSmushButton = document.createElement("button");
-miceSmushButton.innerHTML = "Smush a mouse 🐁";
-app.append(miceSmushButton);
-
-app.append(document.createElement("br"));
-app.append(document.createElement("br"));
-
-const bottomUpgradeContainer = document.createElement("div");
-bottomUpgradeContainer.setAttribute(
-  "style",
-  "height: 200px; position: fixed; bottom:2%;width:100%;display:flex; flex-wrap:wrap;justify-content:center;",
-);
-document.body.append(bottomUpgradeContainer);
-
 interface Upgrade {
   name: string;
   cost: number;
   button: null | HTMLButtonElement;
   rateText: null | HTMLParagraphElement;
+  purchasedText: null | HTMLSpanElement;
   purchased: number;
   rate: number;
 }
 
 const upgrades: Upgrade[] = [
   {
-    name: "Auto-Pounder",
+    name: "Drinking Bird",
     cost: 10,
     button: null,
     rateText: null,
+    purchasedText: null,
     purchased: 0,
     rate: 0.1,
   },
   {
-    name: "Auto-Pounder",
-    cost: 10,
+    name: "Meat Pounder",
+    cost: 100,
     button: null,
     rateText: null,
+    purchasedText: null,
     purchased: 0,
-    rate: 0.1,
+    rate: 2,
+  },
+  {
+    name: "Hydralic Press",
+    cost: 1000,
+    button: null,
+    rateText: null,
+    purchasedText: null,
+    purchased: 0,
+    rate: 50,
   },
 ];
 
-upgrades.forEach((upgrade) => {
-  const box = document.createElement("div");
-  box.setAttribute(
+let bottomUpgradeContainer: HTMLDivElement;
+let miceSmushButton: HTMLButtonElement;
+let totalRate: HTMLHeadingElement;
+let smushCounter: HTMLDivElement;
+
+function SetupApp() {
+  const header = document.createElement("div");
+  header.setAttribute("id", "head");
+  header.setAttribute(
     "style",
-    "text-align:center; flex; 2 2;transform:translateY(30%);",
+    "vertical-align: text-top;font-size: 1em;text-align: center;",
   );
+  document.body.prepend(header);
+  const headerText = document.createElement("h1");
+  headerText.innerHTML = gameName;
+  header.append(headerText);
 
-  const rateText = document.createElement("p");
-  upgrade.rateText = rateText;
-  rateText.textContent = "0/sec";
-  box.append(rateText);
+  const centerContainer = document.createElement("div");
+  app.append(centerContainer);
 
-  const upgradeButton = document.createElement("button");
-  upgrade.button = upgradeButton;
-  upgradeButton.disabled = true;
-  upgradeButton.innerHTML = upgrade.name;
-  upgradeButton.setAttribute("style", "text-align:left;font-size:23px;");
-  const cost = document.createElement("h3");
-  cost.setAttribute(
+  smushCounter = document.createElement("div");
+  smushCounter.style.fontSize = "35px";
+  smushCounter.innerHTML = "0";
+  centerContainer.append(smushCounter);
+
+  app.append(document.createElement("br"));
+
+  miceSmushButton = document.createElement("button");
+  miceSmushButton.innerHTML = "Smush a mouse 🐁";
+  app.append(miceSmushButton);
+
+  totalRate = document.createElement("h3");
+  totalRate.innerHTML = "0/sec";
+  app.append(totalRate);
+
+  bottomUpgradeContainer = document.createElement("div");
+  bottomUpgradeContainer.setAttribute(
     "style",
-    "color:red;margin-top:-3px;margin-bottom:-3px;font-size:18px",
+    "position: absolute; bottom:5%;width:100%;display:flex; flex-wrap:wrap;justify-content:center;gap:10px;",
   );
-  cost.innerHTML = "Buy for " + upgrade.cost.toString();
-  upgradeButton.append(cost);
-  box.append(upgradeButton);
+  document.body.append(bottomUpgradeContainer);
+}
 
-  bottomUpgradeContainer.append(box);
-});
+function SetupUpgrades() {
+  upgrades.forEach((upgrade) => {
+    const box = document.createElement("div");
+    box.setAttribute(
+      "style",
+      "text-align:center; flex; 2 2;transform:translateY(30%);",
+    );
 
-let counter: number = 0;
+    const rateText = document.createElement("p");
+    upgrade.rateText = rateText;
+    rateText.textContent = "0/sec";
+    rateText.setAttribute("style", "text-align:center;");
+    box.append(rateText);
+
+    const upgradeButton = document.createElement("button");
+    upgrade.button = upgradeButton;
+    upgradeButton.disabled = true;
+    upgradeButton.innerHTML = upgrade.name;
+    upgradeButton.setAttribute(
+      "style",
+      "text-align:left;font-size:23px;padding-right:60px;",
+    );
+
+    const cost = document.createElement("h3");
+    cost.setAttribute(
+      "style",
+      "color:red;margin-top:-3px;margin-bottom:-3px;font-size:18px;",
+    );
+    cost.innerHTML = "$" + upgrade.cost.toString();
+    upgradeButton.append(cost);
+
+    const rateAmount = document.createElement("span");
+    rateAmount.textContent = "(" + upgrade.rate.toFixed(1) + "x)";
+    rateAmount.setAttribute(
+      "style",
+      "margin-top:10px;margin-left:10px;color:gray;font-size:14px;",
+    );
+    cost.append(rateAmount);
+
+    const amountPurchased = document.createElement("span");
+    amountPurchased.textContent = "0";
+    amountPurchased.setAttribute(
+      "style",
+      "color:white;float:right;margin-top:-15px;margin-right: -35px;",
+    );
+    upgrade.purchasedText = amountPurchased;
+    cost.append(amountPurchased);
+    box.append(upgradeButton);
+
+    bottomUpgradeContainer.append(box);
+  });
+
+  // Set up upgrade features
+
+  upgrades.forEach((upgrade) => {
+    upgrade.button?.addEventListener("click", () => {
+      totalSmushes -= upgrade.cost;
+      upgrade.purchased++;
+      if (upgrade.purchasedText)
+        upgrade.purchasedText.textContent = upgrade.purchased.toFixed(0);
+      if (upgrade.rateText)
+        upgrade.rateText.textContent =
+          (upgrade.rate * upgrade.purchased).toFixed(1) + "/sec";
+
+      autoGrowth += upgrade.rate;
+      totalRate.innerHTML = autoGrowth.toFixed(2) + "/sec";
+    });
+  });
+}
 
 function incrementCounter(amount: number) {
-  counter += amount;
-  smushCounter.innerHTML = counter.toFixed(2);
+  totalSmushes += amount;
+  smushCounter.innerHTML = totalSmushes.toFixed(2);
 }
-miceSmushButton.addEventListener("click", () => incrementCounter(1));
 
-requestAnimationFrame(update);
-//requestAnimationFrame(checkForUpgradeOne);
+function main() {
+  SetupApp();
+  SetupUpgrades();
 
-const growthRate = 0;
+  miceSmushButton.addEventListener("click", () => incrementCounter(1));
 
-let lastTimestamp: number = 0;
-function update(timestamp: number) {
-  incrementCounter((growthRate * (timestamp - lastTimestamp)) / 1000);
-  lastTimestamp = timestamp;
   requestAnimationFrame(update);
 }
 
-// function checkForUpgradeOne() {
-//   if (counter >= 10) {
-//     upgradeOneButton.disabled = false;
-//   } else requestAnimationFrame(checkForUpgradeOne);
-// }
-// upgradeOneButton.addEventListener("click", () => {
-//   counter -= 10;
-//   growthRate++;
-//   if (counter < 10) {
-//     upgradeOneButton.disabled = true;
-//     requestAnimationFrame(checkForUpgradeOne);
-//   }
-//   upgradeOneAmount.textContent = `AUTO-POUNDER x${growthRate}`;
-// });
+let totalSmushes: number = 0;
+let autoGrowth: number = 0;
+let lastTimestamp: number = 0;
+function update(timestamp: number) {
+  incrementCounter((autoGrowth * (timestamp - lastTimestamp)) / 1000);
+  lastTimestamp = timestamp;
+  UpdateUpgrades();
+  requestAnimationFrame(update);
+}
+
+function UpdateUpgrades() {
+  upgrades.forEach((upgrade) => {
+    if (upgrade.button != null) {
+      upgrade.button.disabled = totalSmushes < upgrade.cost;
+    }
+  });
+}
+
+main();
